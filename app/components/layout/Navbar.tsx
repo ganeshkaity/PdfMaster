3ab7153f26ff171863923e62c655d7678b9c0742
Ganesh <ganeshkaitycodechannel@gmail.com>
2026-01-08 02:41:55 +0530

    Remove default favicon.ico to allow icon.png (logo) to be used as favicon

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -23,10 +23,6 @@ export default function Navbar() {
         <nav className="sticky top-0 z-50 w-full glass border-b border-white/10">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
-                    import Image from "next/image";
-
-                    // ... (existing imports)
-
                     <Link href="/" className="flex items-center space-x-3 group">
                         <div className="relative w-10 h-10 overflow-hidden rounded-xl group-hover:shadow-[0_0_20px_rgba(6,182,212,0.5)] transition-all">
                             <Image
